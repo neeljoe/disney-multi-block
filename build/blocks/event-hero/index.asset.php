103f00dcf0f5cf4a0de05afcd70d2c383d9e28c7
neeljoe <joe.neel@gmail.com>
2026-06-02 11:01:03 +0530

    front page

--- build/blocks/event-hero/index.asset.php
+++ build/blocks/event-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b90b6e8778f3731b4f20');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '40c969c18c0ca32b4260');
